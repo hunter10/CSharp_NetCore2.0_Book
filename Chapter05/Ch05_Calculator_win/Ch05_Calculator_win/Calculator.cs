0a384f8cf1aeb0330281250fe96dd147ef09f139
hunter10 <hunter10@naver.com>
2019-02-10 15:15:19 +0900

    Chapter5 과정 추가, Unit Test 방법 간단히

---
+++ Chapter05/Ch05_Calculator_win/Ch05_Calculator_win/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ch05_Calculator_win
+{
+    public class Calculator
+    {
+        public double Add(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
